a365a2f986c8ab97de344532924b628c6cef47dd
Jonny Tullis <jon.tullis558@gmail.com>
2019-11-05 00:19:10 +0000

    Backend working for /getjokes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile('index.html', { root: 'public' });
+});
+
+/* GET jokes. */
+router.get('/getjokes', function(req, res, next) {
+    console.log("in 'getjoke' endpoint")
+    fs.readFile(__dirname + '/jokes.txt', function(err, data) {
+        if (err) throw err;
+        var jokes = data.toString().split("\n");
+        for (var i = 0; i < jokes.length; i++) {
+            console.log(jokes[i]);
+        }
+    });
+    
+    
 });
 
 module.exports = router;
